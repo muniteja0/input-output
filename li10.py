dcd13ad656391276a4e2de1d8e8b97d9e415c27f
muniteja0 <22hr1a0557@mtieat.org>
2024-10-03 08:51:34 +0530

    Create li10.py

---
+++ li10.py
@@ -0,0 +1,18 @@
+'''
+Write a program to sort the given list and print it.
+
+Sample Input:
+30 20 10 50 40
+
+Sample Output:
+10 20 30 40 50
+'''
+
+
+elements = list(map(int, input().split()))
+
+
+elements.sort()
+
+
+print(' '.join(map(str, elements)))
